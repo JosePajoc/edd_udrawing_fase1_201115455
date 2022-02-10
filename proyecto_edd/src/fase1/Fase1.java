bd1fbc5d7f5c93fe6a9efb476f157d21d9b74359
JosePajoc <74215060+JosePajoc@users.noreply.github.com>
2022-02-10 17:57:13 -0600

    Lectura de archivo Json a través de librería JsonSimple

--- proyecto_edd/src/fase1/Fase1.java
+++ proyecto_edd/src/fase1/Fase1.java
@@ -1,6 +1,13 @@
 package fase1;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Fase1 {
 
@@ -9,7 +16,7 @@ public class Fase1 {
         String rutaJson;
         boolean jsonCarga = false, ventanillaCarga = false;
         int ventanillasNum;
-        do{
+        do {
             try {
                 System.out.println("------------------------------------------------------");
                 System.out.println("1. Parámetros iniciales");
@@ -21,18 +28,44 @@ public class Fase1 {
                 System.out.println("Ingrese el número de opción: ");
                 Scanner entrada1 = new Scanner(System.in);
                 op = entrada1.nextInt();
-                if(op == 1){
+                if (op == 1) {
                     System.out.println("\ta. Carga masiva clientes");
                     System.out.println("\tb. Cantidad de ventanillas\n");
                     System.out.println("\tIngrese la literal de la opción: ");
                     Scanner entrada2 = new Scanner(System.in);
-                    char op2 = entrada2.nextLine().charAt(0); 
+                    char op2 = entrada2.nextLine().charAt(0);
                     switch (op2) {
                         case 'a':
                             System.out.println("\t\tIngrese la ruta del archivo JSON que posee los datos: ");
                             rutaJson = entrada2.nextLine();
                             System.out.println("\t\tLa ruta es-> " + rutaJson);
-                            jsonCarga = true;
+                            try {
+                                //Crear objeto JsonParser, se usa para almacenar el json externo
+                                JSONParser convertidor = new JSONParser();
+                                //Objeto generico para manipular el json externo
+                                Object objeto = convertidor.parse(new FileReader(rutaJson));
+                                //Hacer casteo al objeto generico y convertirlo a Objeto JSON
+                                JSONObject entradaJson = (JSONObject) objeto;
+                                for (int i = 1; i <= entradaJson.size(); i++) {
+                                    //Iterando para cada cliente del Json
+                                    Map datos = ((Map) entradaJson.get("Cliente" + i));
+                                    System.out.println("--------------------------");
+                                    //Se crea objeto para acceder a los datos del mapa en iteraciones
+                                    Iterator<Map.Entry> info = datos.entrySet().iterator();
+                                    //Mientras que datos posea valor
+                                    while (info.hasNext()) {
+                                        //A cada valor lo asignamos a una variable que poseerá Clave y valor
+                                        Map.Entry par = info.next();
+                                        //Mostrar cada par
+                                        System.out.println(par.getKey() + ":" + par.getValue());
+                                    }
+                                }
+                                jsonCarga = true;
+                            } catch (IOException e) {
+
+                            } catch (ParseException e) {
+
+                            }
                             break;
                         case 'b':
                             System.out.println("\t\tIngrese el número de ventanillas: ");
@@ -44,18 +77,18 @@ public class Fase1 {
                             System.out.println("Opción no valida");
                             break;
                     }
-                }else if(op == 2){
-                    if(jsonCarga && ventanillaCarga){
+                } else if (op == 2) {
+                    if (jsonCarga && ventanillaCarga) {
                         System.out.println("-------------------------> EJECUTANDO PASO <-------------------------");
-                    }else{
+                    } else {
                         System.out.println("Verificar si se cargaron datos de los clientes y de las ventanillas");
                     }
                 }
-            }catch(Exception error){
+            } catch (Exception error) {
                 System.out.println("---> El valor ingresado es incorrecto <---");
             }
 
-        }while (op != 6);
+        } while (op != 6);
     }
 
 }
