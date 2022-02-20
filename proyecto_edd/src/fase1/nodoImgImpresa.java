e81fcb8421721f6229668bed2483a7a9fbf2f709
JosePajoc <74215060+JosePajoc@users.noreply.github.com>
2022-02-20 17:28:43 -0600

    Manuales e información complementaria

--- proyecto_edd/src/fase1/nodoImgImpresa.java
+++ proyecto_edd/src/fase1/nodoImgImpresa.java
@@ -4,6 +4,7 @@ public class nodoImgImpresa {
     String tipoImg;
     nodoImgImpresa siguiente;
     
+    //Nodo de tipo imagen para ser agregada al cliente que esta en la sala de espera
     public nodoImgImpresa(String tipoImg){
         this.tipoImg = tipoImg;
         this.siguiente = null;
